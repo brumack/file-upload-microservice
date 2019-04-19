a9787d1bade81e8a42c3a3a63bdb6a9c49ea617b
Glitch (iron-rain) <none>
2019-04-19 19:32:46 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 // require and use "multer"...
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+   res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  const { originalname, mimetype, size } = req.file
+  res.json({name: originalname, type: mimetype, size})
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
